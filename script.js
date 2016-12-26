f38799d0dc7965895e7ce093d9a45b7cbc69d9e0
gous32 <alex.gusev.32@gmail.com>
2016-12-26 21:16:43 +0300

    score

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 console.log('Hi!');
 
+var score = 0;
+var $score = $('.score');
+var $scoreVal = $('.score-body');
+
+function correctAnswer() {
+  score += 100;
+  $scoreVal[0].innerHTML = score;
+}
+
+function incorrectAnswer() {
+  score -= 100;
+  $scoreVal[0].innerHTML = score;
+  if (score < 0) {
+    $score.removeClass('btn-success').addClass('btn-danger');
+    $('.loose').modal('show');
+  }
+}
+
+function doWin() {
+  $('#rname')[0].innerHTML = $('#name').val();
+  $('#rdepartment')[0].innerHTML = $('#department').val();
+  $('#rcourse')[0].innerHTML = $('#year').val();
+  $('#rgroup')[0].innerHTML = $('#group').val();
+  $('#rscore')[0].innerHTML = score;
+  $('#rcode')[0].innerHTML = '17DD0A93';
+
+  $('.win').modal('show');
+}
+
 $('#start-game').click(function() {
   $('.button-row').hide(1000);
   $('#common .content').collapse('hide')
@@ -18,7 +47,7 @@ function startGame() {
 }
 
 
-function createAnswerFunction(idQuestion, node, correctNum, idNext) {
+function createAnswerFunction(idQuestion, node, correctNum, idNext, bigNum) {
   return function(ev) {
     var $question = $('#' + idQuestion);
     var $buttons = $('button', $question);
@@ -26,8 +55,10 @@ function createAnswerFunction(idQuestion, node, correctNum, idNext) {
 
     if ($(this).attr('data-num') === '' + correctNum) {
       $question.removeClass('panel-default').addClass('panel-success');
+      scores[bigNum] = true;
     } else {
       $question.removeClass('panel-default').addClass('panel-danger');
+      scores[bigNum] = scores[bigNum] || false;
     }
 
     $buttons.filter('[data-num=' + correctNum + ']').addClass('btn-success');
@@ -39,7 +70,10 @@ function createAnswerFunction(idQuestion, node, correctNum, idNext) {
 
 var counter = 1;
 
-function createTestQuestion(data) {
+function createTestQuestion(data, bigNum, isLast) {
+  if (counter !== 1) {
+    $('.imgg').remove();
+  }
   var $clone = $('#text-example').clone();
   var id = 'q' + counter;
   counter++;
@@ -58,14 +92,23 @@ function createTestQuestion(data) {
     $('button', $btnClone).attr('data-num', i);
 
     $('button', $btnClone)
-      .click(createAnswerFunction(id, $('button', $btnClone)[0], data.correct, 'q' + counter));
+      .click(createAnswerFunction(id, $('button', $btnClone)[0], data.correct, 'q' + counter, bigNum));
+    if (isLast) {
+      $('button', $btnClone).click(function() {
+        if (scores[bigNum] === true) {
+          correctAnswer();
+        } else {
+          incorrectAnswer();
+        }
+      });
+    }
   }
   $btn.remove();
   $clone.insertBefore($('#text-example'));
   $('.questions-container').append($clone);
 }
 
-function createBigQuestion(correct) {
+function createBigQuestion(correct, bigNum, isLast) {
   var $node = $('#q' + counter);
   var $input = $('input', $node);
   var $btn = $('button', $node);
@@ -77,14 +120,19 @@ function createBigQuestion(correct) {
     if ('' + val === '' + correct) {
       $node.removeClass('panel-default').addClass('panel-success');
       $btn.removeClass('btn-default').addClass('btn-success');
+      correctAnswer();
     } else {
       $node.removeClass('panel-default').addClass('panel-danger');
       $btn.removeClass('btn-default').addClass('btn-danger');
+      incorrectAnswer();
     }
 
     $btn.attr('disabled', 'disabled');
-    console.log(next);
     $('#q' + next + ' .content').show(500);
+
+    if (isLast && score >= 0) {
+      doWin();
+    }
   });
   $('.questions-container').append($node);
 }
@@ -487,12 +535,14 @@ datas4 = [
   }
 ]
 
+var scores = [];
+
 
 createTestQuestion(datas[0], 0);
 createTestQuestion(datas[1], 0);
 createTestQuestion(datas[2], 0);
 createTestQuestion(datas[3], 0);
-createTestQuestion(datas[4], 0);
+createTestQuestion(datas[4], 0, true);
 
 createBigQuestion(17177, 1);
 
@@ -501,7 +551,7 @@ createTestQuestion(datas2[1], 2);
 createTestQuestion(datas2[2], 2);
 createTestQuestion(datas2[3], 2);
 createTestQuestion(datas2[4], 2);
-createTestQuestion(datas2[5], 2);
+createTestQuestion(datas2[5], 2, true);
 
 createBigQuestion(68875, 4);
 createBigQuestion(939, 5);
@@ -510,7 +560,7 @@ createTestQuestion(datas3[0], 6);
 createTestQuestion(datas3[1], 6);
 createTestQuestion(datas3[2], 6);
 createTestQuestion(datas3[3], 6);
-createTestQuestion(datas3[4], 6);
+createTestQuestion(datas3[4], 6, true);
 
 createBigQuestion(927, 7);
 createBigQuestion(310, 8);
@@ -519,6 +569,6 @@ createTestQuestion(datas3[0], 9);
 createTestQuestion(datas3[1], 9);
 createTestQuestion(datas3[2], 9);
 createTestQuestion(datas3[3], 9);
-createTestQuestion(datas3[4], 9);
+createTestQuestion(datas3[4], 9, true);
 
-createBigQuestion(6, 10);
+createBigQuestion(6, 10, true);
